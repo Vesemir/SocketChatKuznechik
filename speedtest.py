a70f5b5bc59173740bca9c65692866526c17b4dd
Vasya <somemail@anywhere.com>
2015-10-28 22:03:39 +0300

    Merged one file

--- speedtest.py
+++ speedtest.py
@@ -4,13 +4,15 @@ from galois import message_encrypt
 from galois import message_decrypt
 from galois import make_keys
 import timeit
+import matplotlib.pyplot as plt
 
 choices = '0123456789abcdef'
 
 KEY = '01234567890123456789012345678901'
 SIZE = 32
 KEYS = make_keys(KEY)
-REPEATS = 200
+
+REPEATS = 1000
 
 
 def string_yielder():
@@ -27,8 +29,14 @@ def get_string(size=SIZE):
 def avg(numbers):
     return sum(numbers) / len(numbers)
 
-tring = get_string()
-tr = timeit.Timer('message_encrypt(KEYS, tring)', 'from __main__ import KEYS, tring, message_encrypt')
-res = tr.repeat(repeat=REPEATS, number=1)
-print("Timing with block size %d, for %d repeats, got result %f " %(SIZE, REPEATS, avg(res)))
+if __name__ == '__main__':
+    plotbuff = []
+    for strlen in range(32, 128, 32):
+        tring = get_string(strlen)
+        tr = timeit.Timer('message_encrypt(KEYS, tring)', 'from __main__ import KEYS, tring, message_encrypt')
+        res = tr.repeat(repeat=REPEATS, number=1)
+        print("Timing with block size %d, for %d repeats, got result %f " %(strlen, REPEATS, avg(res)))
+        plotbuff.append((strlen, res))
+    plt.plot(list(zip(*plotbuff)))
+    plt.show()
 
