fc624db6ce4ff9fc93b9e2f705264c1c1b99713b
Vasya <somemail@anywhere.com>
2015-10-30 23:14:58 +0300

    Sudoky

--- speedtest.py
+++ speedtest.py
@@ -31,12 +31,14 @@ def avg(numbers):
 
 if __name__ == '__main__':
     plotbuff = []
-    for strlen in range(1632, 1633, 32):
+    for strlen in range(32, 257, 32):
         tring = get_string(strlen)
         tr = timeit.Timer('message_encrypt(KEYS, tring)', 'from __main__ import KEYS, tring, message_encrypt')
         res = tr.repeat(repeat=REPEATS, number=1)
         print("Timing with block size %d, for %d repeats, got result %f " %(strlen, REPEATS, avg(res)))
-        plotbuff.append((strlen, res))
-    plt.plot(list(zip(*plotbuff)))
+        plotbuff.append((strlen, avg(res)))
+    print(plotbuff)
+    so = list(zip(*plotbuff))
+    plt.plot(so[0], so[1])
     plt.show()
 
